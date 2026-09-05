1d40c792dc64c1f2077f7c234acecdd543874a58
jorgelucas-byuprojects <jorgitoieln@gmail.com>
2026-09-05 17:20:02 -0400

    act 3 week1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,23 @@
-using System;
+Random randomGenerator = new Random();
+int magicNumber = randomGenerator.Next(1, 101);
 
-class Program
+int guess = -1;
+
+while (guess != magicNumber)
 {
-    static void Main(string[] args)
+    Console.Write("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+
+    if (magicNumber > guess)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Higher");
+    }
+    else if (magicNumber < guess)
+    {
+        Console.WriteLine("Lower");
+    }
+    else
+    {
+        Console.WriteLine("You guessed it!");
     }
 }
