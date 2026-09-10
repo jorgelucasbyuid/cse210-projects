bbe57aace4017862269ed15be3942a1f4afe50ca
jorgelucas-byuprojects <jorgitoieln@gmail.com>
2026-09-10 18:11:23 -0300

    Jounal.cs

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+    }
+
+    public void DisplayAll()
+    {
+    }
+
+    public void SaveToFile(string file)
+    {
+    }
+
+    public void LoadFromFile(string file)
+    {
+    }
+}
