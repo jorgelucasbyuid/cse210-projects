9fdd98a2b4dc582d34d5d16c728215be8799bf12
jorgelucas-byuprojects <jorgitoieln@gmail.com>
2026-09-12 19:37:24 -0300

    Act

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,10 +1,19 @@
+using System;
+
 public class Entry
 {
-    private string _date;
-    private string _promptText;
-    private string _entryText;
+    public string _date = "";
+    public string _promptText = "";
+    public string _entryText = "";
+    public string _mood = "";
 
     public void Display()
     {
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
+        if (!string.IsNullOrEmpty(_mood))
+        {
+            Console.WriteLine($"Mood: {_mood}");
+        }
+        Console.WriteLine($"Entry: {_entryText}\n");
     }
 }
